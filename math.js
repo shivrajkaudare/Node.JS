efea4d10911fdb83395b162981f9d1ffd2b23778
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-04-11 15:15:59 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+const mul = (a,b) => a * b;
+const g = 9.15;
+const PI = 3.14;
+
+let obj = {
+      sum : sum,
+      mul : mul,
+      g : g,
+      PI : PI,
+};
+
+module.exports = obj;
