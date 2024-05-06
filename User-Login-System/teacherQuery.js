a1fe57962532f87d3e535695e59ca5d8e7b23f0d
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-06 22:15:50 +0530

    Create teacherQuery.js

---
+++ User-Login-System/teacherQuery.js
@@ -0,0 +1,47 @@
+const mysql = require("mysql");
+
+// Create a connection to the database
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  user: "your_username",
+
+  password: "your_password",
+
+  database: "your_database",
+});
+
+// Connect to the database
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+
+    return;
+  }
+
+  console.log("Connected to database");
+});
+
+// Select all records from the Teacher table where salary is greater than 20,000
+
+const sqlQuery = "SELECT * FROM Teacher WHERE salary > 20000";
+
+// Execute the SQL query
+
+connection.query(sqlQuery, (err, results) => {
+  if (err) {
+    console.error("Error executing query:", err);
+
+    return;
+  }
+
+  console.log("Teachers with salary greater than 20,000:");
+
+  console.log(results);
+});
+
+// Close the connection
+
+connection.end();
