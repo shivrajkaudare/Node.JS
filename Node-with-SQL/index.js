131a2fcf734598e671266617825d829d82d00093
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-06-07 11:27:42 +0530

    Update index.js

--- Node-with-SQL/index.js
+++ Node-with-SQL/index.js
@@ -1,7 +1,5 @@
-const { faker } = require("@faker-js/faker");
-
 // require MY SQL pakage
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 // creating connection of node with my sql. i.e. // Create the connection to database
 const connection = mysql.createConnection({
@@ -10,14 +8,33 @@ const connection = mysql.createConnection({
   database: "user_data",
   password: "Shivraj1234",
 });
+
+// Insert New data into table.
+// ? - this is placeholders
+let q = "INSERT INTO user (id, username, email, password) VALUES ? ";
+// Insert single user
+// let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
+// Insert multiple users.
+let users = [
+  ["124", "124_shiv", "abcd@gmail.com", "shsj"],
+  ["125", "125_raj", "raj@gmail.com", "assl"],
+];
+
 try {
-  connection.query("SHOW TABLE", (err, result) => {
+  connection.query(q, /*user*/ [users], (err, result) => {
     if (err) throw err;
     console.log(result);
   });
+  // ----- OR ------
+  // try {
+  //   connection.query("SHOW TABLES", (err, result) => {
+  //     if (err) throw err;
+  //     console.log(result);
+  //   });
 } catch (err) {
   console.log(err);
 }
+connection.end();
 
 let getRandomUser = () => {
   return {
